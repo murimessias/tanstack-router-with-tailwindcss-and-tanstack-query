02ff051202a2765b592167dfb2301f4775b5d931
Murilo Messias de Camargo <murilo.messias@gmail.com>
2025-05-30 18:39:48 -0300

    feat: setup tailwindcss

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,34 +1,38 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-import '#app/styles/app.css'
-
 export const Route = createFileRoute('/')({
-	component: App,
+	component: RouteComponent,
 })
 
-function App() {
+function RouteComponent() {
 	return (
-		<div className="App">
-			<header className="App-header">
+		<div className="grid h-dvh w-full place-content-center">
+			<header className="text-center">
+				<h1 className="mb-10 text-5xl font-bold">Tanstack React Router</h1>
+
 				<p>
-					Edit <code>src/routes/index.tsx</code> and save to reload.
+					Edit <code className="bg-zinc-100 p-1">app/routes/index.tsx</code> and
+					save to reload.
 				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<a
-					className="App-link"
-					href="https://tanstack.com"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn TanStack
-				</a>
+
+				<div className="mt-4 flex items-center justify-center gap-4">
+					<a
+						className="inline-flex min-h-10 items-center rounded bg-zinc-600 px-4 text-white"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
+					</a>
+					<a
+						className="inline-flex min-h-10 items-center rounded bg-zinc-600 px-4 text-white"
+						href="https://tanstack.com"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn TanStack
+					</a>
+				</div>
 			</header>
 		</div>
 	)
