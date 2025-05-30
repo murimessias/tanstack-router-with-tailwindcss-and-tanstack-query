f1d0e1401fe88b1f494ed68e6eba5e89f4ef6ab1
Murilo Messias de Camargo <murilo.messias@gmail.com>
2025-05-30 19:03:14 -0300

    feat: setup tanstack query

--- app/root.tsx
+++ app/root.tsx
@@ -1,19 +1,21 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-
+import { queryClient } from '#app/utils/query-client.ts'
 import { routeTree } from '../types/route-tree.gen.ts'
 import reportWebVitals from './reportWebVitals.ts'
-
 import '#app/styles/tailwind.css'
 
 const router = createRouter({
 	routeTree,
-	context: {},
+	context: {
+		queryClient,
+	},
 	defaultPreload: 'intent',
-	scrollRestoration: true,
 	defaultStructuralSharing: true,
 	defaultPreloadStaleTime: 0,
+	scrollRestoration: true,
 })
 
 declare module '@tanstack/react-router' {
@@ -22,13 +24,21 @@ declare module '@tanstack/react-router' {
 	}
 }
 
+function AppWithProviders() {
+	return (
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+		</QueryClientProvider>
+	)
+}
+
 const rootElement = document.querySelector('[data-js="root"]')
 if (!rootElement) throw new Error('Failed to find the root element')
 
 const root = ReactDOM.createRoot(rootElement)
 root.render(
 	<StrictMode>
-		<RouterProvider router={router} />
+		<AppWithProviders />
 	</StrictMode>,
 )
 
--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -1,10 +1,15 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { type QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
-export const Route = createRootRoute({
+export const Route = createRootRouteWithContext<{
+	queryClient: QueryClient
+}>()({
 	component: () => (
 		<>
 			<Outlet />
+			<ReactQueryDevtools />
 			<TanStackRouterDevtools />
 		</>
 	),
